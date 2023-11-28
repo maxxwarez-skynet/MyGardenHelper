e1c12b950398064ea7a79c4a6afb378b43a26cab
abhinav.n <abhinav.n>
2023-11-28 12:57:21 +0530

    1) Update functions. Create getUser api to return user object. 2) Update app. Firebase auth.

---
+++ app/src/main/java/in/co/maxxwarez/mygardenhelper/helperClasses/userHelper.java
@@ -0,0 +1,86 @@
+package in.co.maxxwarez.mygardenhelper.helperClasses;
+
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+
+public class userHelper {
+    private static final String TAG = "SkyNet";
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    final DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+    public HashMap<String, HashMap<String, String>> userObject;
+
+    public userHelper() {
+    }
+
+    public String getUID () {
+        return user.getUid();
+    }
+
+    public String getEmailID () {
+        return user.getEmail();
+    }
+
+    public String getDisplayName () {
+        return user.getDisplayName();
+    }
+
+    public HashMap getHome(){ return getUser();}
+
+    public boolean isLoggedIn () {
+        if (user != null)
+            return true;
+        else
+            return false;
+    }
+
+    public void createUser () {
+        Query query = ref.child("users").child(this.getUID());
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (!dataSnapshot.exists()){
+                    ref.child("users").child(user.getUid()).child("name").setValue(user.getDisplayName());
+                    ref.child("users").child(user.getUid()).child("email").setValue(user.getEmail());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public HashMap<String, HashMap<String, String>> getUser () {
+        Query query = ref.child("users").child(this.getUID()).child("homes");
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    HashMap <String, HashMap<String, String>> userObj = new HashMap<String, HashMap<String,String>>();
+                    userObj = (HashMap<String, HashMap<String, String>>) dataSnapshot.getValue();
+                    userObject = (HashMap<String, HashMap<String, String>>) dataSnapshot.getValue();
+                    Log.i(TAG, "HashMap userHelper " + userObject.toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        return userObject;
+    }
+}
---
+++ functions/functions/index.js
@@ -0,0 +1,39 @@
+const {logger} = require("firebase-functions");
+const {onRequest} = require("firebase-functions/v2/https");
+const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+const { getAuth } = require("firebase-admin/auth");
+
+// The Firebase Admin SDK to access Firestore.
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
+const { user } = require("firebase-functions/v1/auth");
+
+initializeApp();
+const db = getFirestore();
+
+// Take the text parameter passed to this HTTP endpoint and query it from
+// Firestore under the path /users/
+exports.getUser = onRequest(async (req, res) => {
+
+  // Grab the text parameter.
+  const userID = req.query.userID;
+
+  //Get user from Firestore
+  const userRef = db.collection('users').doc(userID);
+  const doc = await userRef.get();
+
+  if (!doc.exists) {
+    const user = await getAuth().getUser(userID);
+    const userData = {
+        email   : user.email,
+        name    : user.displayName
+    }
+    const res = await db.collection('users').doc(userID).set(userData);
+
+  } else {
+    
+  }
+  res.send('true');
+});
+
+
